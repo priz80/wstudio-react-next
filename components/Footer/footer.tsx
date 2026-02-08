c5ec476e9bd1352de3112a9c47f057640fd439e5
priz80 <priz8090@gmail.com>
2026-02-08 22:41:55 +0300

    Create footer.tsx

---
+++ components/Footer/footer.tsx
@@ -0,0 +1,10 @@
+import "./footer.scss";
+
+export const Footer = () => {
+    return(
+        <div className="footer-block">
+            <p>© 2022–2026, WEB Studio - Разработка сайтов для предприятий</p>
+            <p>Политика сайта и обработка персональных данных</p>
+        </div>
+    )
+}
