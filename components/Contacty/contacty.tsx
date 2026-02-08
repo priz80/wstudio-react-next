87db8fb1d4970fb998615856402d942c1605ca8f
priz80 <priz8090@gmail.com>
2026-02-08 22:41:50 +0300

    Create contacty.tsx

---
+++ components/Contacty/contacty.tsx
@@ -0,0 +1,20 @@
+import "./contacty.scss";
+
+export const Contacty = () => {
+  return (
+    <>
+      <div className="title-contacty">
+        <h6>Контакты</h6>
+      </div>
+      <div className="ball-block_contacty">
+        <div className="ball-circle">
+            <div className="ball">
+                <div className="ball-icon">
+                    
+                </div>
+            </div>
+        </div>
+      </div>
+    </>
+  );
+};
