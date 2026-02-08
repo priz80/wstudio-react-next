e6c8dc06c2838828f2b5e66526b758566a4755e2
priz80 <priz8090@gmail.com>
2026-02-08 22:41:31 +0300

    Delete button.tsx

--- app/button.tsx
+++
@@ -1,9 +0,0 @@
-import "./button.scss";
-
-const Button = ( { sizeButton = "medium", titleButton = "Кнопка"}) => {
-    return (
-        <button type="button" className={`button button-font ${sizeButton}`}>{titleButton}</button>
-    )
-}
-
-export default Button
