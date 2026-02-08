be09eae08e833926430e1c2bf6e599da5b097520
priz80 <priz8090@gmail.com>
2026-02-08 22:41:43 +0300

    Create button.tsx

---
+++ components/Button/button.tsx
@@ -0,0 +1,9 @@
+
+
+const Button = ( { sizeButton = "medium", titleButton = "Кнопка"}) => {
+    return (
+        <button type="button" className={`button button-font ${sizeButton}`}>{titleButton}</button>
+    )
+}
+
+export default Button
