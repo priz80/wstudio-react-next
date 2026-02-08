dfdb337b45b5e7904b9f8d100cf0b872ed8acf10
priz80 <priz8090@gmail.com>
2026-02-08 22:42:13 +0300

    Create tarif.tsx

---
+++ components/Tarif/tarif.tsx
@@ -0,0 +1,12 @@
+import "./tarif.scss";
+
+export const Tarif = () => {
+    return (
+        <div className="container container-tarif" id="tarif">
+            <div className="aside-line aside-line_tarif"></div>
+            <div className="content">
+                <div className="title-tarif"><h4>Тарифы</h4></div>
+            </div>
+        </div>
+    )
+}
