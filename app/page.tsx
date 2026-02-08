15a84e4f95d91e42ef38001b76e56d772da3fc6f
priz80 <priz8090@gmail.com>
2026-02-08 16:23:01 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,30 @@
 "use client";
 
-import Header from "./header";
+import { Header } from "../public/component/Header/header";
+import { About } from "../public/component/About/about";
+import { Portfolio } from "../public/component/Portfolio/portfolio";
+import { Tarif } from "../public/component/Tarif/tarif";
+import { Forma } from "../public/component/Forma/forma";
+import { Contacty } from "../public/component/Contacty/contacty";
+import { Footer } from "../public/component/Footer/footer";
 
-import "./header.css";
-import "./fonts.css";
-import "./main-menu.css";
-import "./burger.css";
+
+import "./fonts.scss";
+import "./main-menu.scss";
+import "./burger.scss";
+import "./button.scss";
+
 
 export default function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <About />
+      <Portfolio />
+      <Tarif />
+      <Forma />
+      <Contacty />
+      <Footer />
+    </>
+  );
 }
