6153148a4438fcdd0e936d0976a3a05f130f7fe9
priz80 <priz8090@gmail.com>
2026-02-08 22:41:38 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-"use client";
+'use client';
 
-import { Header } from "../public/component/Header/header";
-import { About } from "../public/component/About/about";
-import { Portfolio } from "../public/component/Portfolio/portfolio";
-import { Tarif } from "../public/component/Tarif/tarif";
-import { Forma } from "../public/component/Forma/forma";
-import { Contacty } from "../public/component/Contacty/contacty";
-import { Footer } from "../public/component/Footer/footer";
+import { Header } from "@components/Header/header";
+import { About } from "@components/About/about";
+import { Portfolio } from "@components/Portfolio/portfolio";
+import { Tarif } from "@components/Tarif/tarif";
+import { Forma } from "@components/Forma/forma";
+import { Contacty } from "@components/Contacty/contacty";
+import { Footer } from "@components/Footer/footer";
 
 
 import "./fonts.scss";
