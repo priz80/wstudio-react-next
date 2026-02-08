a12c3f342eff3b67e27e6115a2214d19a819f002
priz80 <priz8090@gmail.com>
2026-02-08 16:23:22 +0300

    Create about.tsx

---
+++ public/component/About/about.tsx
@@ -0,0 +1,33 @@
+import Button from "../../../app/button";
+import "./about.module.scss";
+
+export const About = () => {
+  return (
+    <div className="container container-about"  id="about">
+        <div className="aside-line"></div>
+      <div className="content">
+        <div className="about-title_block">
+          <div className="about-title">
+            <h4>Причины сотрудничать с нами</h4>
+            <p>
+              Пока конкуренты говорят о том, что они лучшие - мы это доказываем
+              делом.
+              <span className="hide-span_about">
+                <br/><br/>Вы можете ознакомиться с информацией о нас и посмотреть наше
+                портфолио
+              </span>
+            </p>
+
+            <Button titleButton="Портфолио" />
+          </div>
+          <div className="about-tile"></div>
+        </div>
+        <div className="about-tile_block">
+          <div className="about-tile"></div>
+          <div className="about-tile"></div>
+          <div className="about-tile"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
