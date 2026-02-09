92a5a1e07e0a782fdd81c257f473fd54fbedace2
priz80 <priz8090@gmail.com>
2026-02-09 09:43:20 +0300

    Update footer.tsx

--- components/Footer/footer.tsx
+++ components/Footer/footer.tsx
@@ -1,10 +1,16 @@
 import "./footer.scss";
 
 export const Footer = () => {
-    return(
-        <div className="footer-block">
-            <p>© 2022–2026, WEB Studio - Разработка сайтов для предприятий</p>
-            <p>Политика сайта и обработка персональных данных</p>
-        </div>
-    )
-}
+  return (
+    <div className="footer-block">
+        <p className="copyright">
+          © 2022–2026, WEB Studio - Разработка сайтов для предприятий
+          <br />
+          <br />
+        </p>
+      <a href="/">
+        <p className="policy">Политика сайта и обработка персональных данных</p>
+      </a>
+    </div>
+  );
+};
