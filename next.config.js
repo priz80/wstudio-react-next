c0f51862e92f2b9860124a129fb0047e280c101a
priz80 <priz8090@gmail.com>
2026-02-09 12:40:33 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   output: 'export', // важно для статической сборки
   basePath: '/wstudio-react-next',
-  assetPrefix: '/wstudio-react-next/',
+  assetPrefix: '/wstudio-react-next/styles/',
 };
 
 module.exports = nextConfig;
+
