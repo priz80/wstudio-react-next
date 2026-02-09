bde2c9fe7391664c12df71405e5dd3a609f452b5
priz80 <priz8090@gmail.com>
2026-02-09 09:22:10 +0300

    Update contacty.tsx

--- components/Contacty/contacty.tsx
+++ components/Contacty/contacty.tsx
@@ -3,18 +3,22 @@ import "./contacty.scss";
 export const Contacty = () => {
   return (
     <>
-      <div className="title-contacty">
-        <h6>Контакты</h6>
-      </div>
-      <div className="ball-block_contacty">
-        <div className="ball-circle">
-            <div className="ball">
-                <div className="ball-icon">
-                    
+      <div className="container">
+        <div className="content">
+          <div className="title-contacty">
+            <h6>Контакты</h6>
+          </div>
+          <div className="ball-block_contacty">
+            <div className="ball-circle">
+                <div className="ball">
+                    <div className="ball-icon">
+                        
+                    </div>
                 </div>
             </div>
+          </div>
         </div>
-      </div>
+      </div >
     </>
   );
 };
