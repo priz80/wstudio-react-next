84a43a2d9c88c582a4cd2f01f6f757c94390e2ab
priz80 <priz8090@gmail.com>
2026-02-09 12:15:56 +0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // важно для статической сборки
+  basePath: '/wstudio-react-next',
+  assetPrefix: '/wstudio-react-next/',
+};
+
+module.exports = nextConfig;
