dc94b1546794a4ba912bdcf7c9de32fbdba76649
priz80 <priz8090@gmail.com>
2026-02-09 13:16:51 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
-      <body>
+      <body className="min-h-screen bg-black text-white">
         {children}
       </body>
     </html>
