5a0c7917d8b19579a1be88270e3db4149204870e
priz80 <priz8090@gmail.com>
2026-02-09 09:22:25 +0300

    Update portfolio.tsx

--- components/Portfolio/portfolio.tsx
+++ components/Portfolio/portfolio.tsx
@@ -2,16 +2,25 @@ import Button from "../Button/button";
 import "./portfolio.scss";
 
 export const Portfolio = () => {
-    return (
-        <div className="container container-portfolio"  id="portfolio">
-            <div className="aside-line aside-line_portfolio"></div>
-            <div className="content">
-                <div className="title-portfolio_block">
-                    <h4>Наше портфолио</h4>
-                    <p>В данном портфолио вы сможете увидеть кейсы наших работ на 2025 - 2026 год</p>
-                    <a href="/"><Button sizeButton="button-portfolio" titleButton="Компьютерные версии"/></a>
-                </div>
-            </div>
+  return (
+    <div className="container container-portfolio" id="portfolio">
+      <div className="aside-line"></div>
+      <div className="content">
+        <div className="title-portfolio_block">
+          <h4>Наше портфолио</h4>
+          <p>
+            В данном портфолио вы сможете увидеть кейсы наших работ на 2025 -
+            2026 год
+          </p>
+            <a href="/">
+              <Button
+                styleButton="button-portfolio"
+                fontButton="button-font_portfolio"
+                nameButton="Компьютерные версии"
+              />
+            </a>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
