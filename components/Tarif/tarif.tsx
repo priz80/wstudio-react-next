a1efe11a061c35d07630a8a0a8622f16c5c56cc1
priz80 <priz8090@gmail.com>
2026-02-09 09:22:29 +0300

    Update tarif.tsx

--- components/Tarif/tarif.tsx
+++ components/Tarif/tarif.tsx
@@ -3,7 +3,7 @@ import "./tarif.scss";
 export const Tarif = () => {
     return (
         <div className="container container-tarif" id="tarif">
-            <div className="aside-line aside-line_tarif"></div>
+            <div className="aside-line"></div>
             <div className="content">
                 <div className="title-tarif"><h4>Тарифы</h4></div>
             </div>
