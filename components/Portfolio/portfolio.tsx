55e4662c9f06aba3ce21807920a6fc8b16a4a787
IGOR <127472053+priz80@users.noreply.github.com>
2026-02-14 23:14:19 +0300

    Update portfolio.tsx

--- components/Portfolio/portfolio.tsx
+++ components/Portfolio/portfolio.tsx
@@ -1,39 +1,9 @@
 import Button from "../Button/button";
 import "./portfolio.scss";
 import "./slider-portfolio.scss";
-import { useState, useRef, useEffect } from "react";
+import "./slider-portfolio.js";
 
 export const Portfolio = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const sliderRef = useRef<HTMLDivElement>(null);
-
-  const totalSlides = 9; // количество слайдов
-  const slideWidth = 672; // ширина одного слайда + отступ (настройте под себя)
-  const visibleSlides = 3; // сколько слайдов видно одновременно
-
-  const nextSlide = () => {
-    if (currentIndex >= totalSlides - visibleSlides) {
-      setCurrentIndex(0); // Сброс к началу
-    } else {
-      setCurrentIndex((prev) => prev + 1);
-    }
-  };
-
-  const prevSlide = () => {
-    if (currentIndex <= 0) {
-      setCurrentIndex(totalSlides - visibleSlides); // Перейти к последней позиции
-    } else {
-      setCurrentIndex((prev) => prev - 1);
-    }
-  };
-
-  // Прокручиваем слайдер при изменении currentIndex
-  useEffect(() => {
-    if (sliderRef.current) {
-      sliderRef.current.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
-    }
-  }, [currentIndex, slideWidth]);
-
   return (
     <div className="container container-portfolio" id="portfolio">
       <div className="aside-line"></div>
@@ -56,26 +26,32 @@ export const Portfolio = () => {
             />
           </a>
         </div>
-        <div className="slider-block">
-          <div className="slider-portfolio_container">
-            <div className="slider" ref={sliderRef}>
-              {Array.from({ length: totalSlides }, (_, i) => (
-                <div className="slide" key={i}>
-                  <img src={`./img/portfolio_img/slide${i + 1}.png`} alt="" />
+        <div className="content">
+          <div className="slider-container">
+            <div className="slider">
+              <div className="slide slide7"></div>
+              <div className="slide slide1"></div>
+              <div className="slide slide2"></div>
+              <div className="slide slide3"></div>
+              <div className="slide slide4"></div>
+              <div className="slide slide5"></div>
+              <div className="slide slide6"></div>
+              <div className="slide slide7"></div>
+              <div className="slide slide1"></div>
+              <div className="slide slide2"></div>
+            </div>
+
+            <div className="button-block">
+              <button id="left" aria-label="Previous slide">
+                <div className="ball">
+                  <img src="./img/arrowleft.svg" alt="Previous" />
                 </div>
-              ))}
-            </div>
-          </div>
-        </div>
-        <div className="arrow-container">
-          <div className="arrow-circle" onClick={prevSlide}>
-            <div className="arrow" id="left">
-              <img src="./img/arrowleft.svg" alt="" />
-            </div>
-          </div>
-          <div className="arrow-circle" onClick={nextSlide}>
-            <div className="arrow">
-              <img src="./img/arrowright.svg" alt="" />
+              </button>
+              <button id="right" aria-label="Next slide">
+                <div className="ball">
+                  <img src="./img/arrowright.svg" alt="Next" />
+                </div>
+              </button>
             </div>
           </div>
         </div>
