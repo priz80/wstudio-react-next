4e8721c32c8c76f1985deae0f35223556a50b719
IGOR <127472053+priz80@users.noreply.github.com>
2026-02-16 01:26:14 +0300

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { Header } from "../components/Header/header";
+import { About } from "../components/About/about";
+import { Portfolio } from "../components/Portfolio/portfolio";
+import { Tarif } from "../components/Tarif/tarif";
+import { Forma } from "../components/Forma/forma";
+import { Contacty } from "../components/Contacty/contacty";
+import { Footer } from "../components/Footer/footer";
+
+
+import "./fonts.scss";
+/* import "./main-menu.scss"; */
+/* import "./burger.scss"; */
+import "./aside-menu.scss";
+
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <About />
+      <Portfolio />
+      <Tarif />
+      <Forma />
+      <Contacty />
+      <Footer />
+    </>
+  );
+}
