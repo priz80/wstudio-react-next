060eb4b389fff0abe9ac5fb958523d2f9a571b9c
IGOR <127472053+priz80@users.noreply.github.com>
2026-02-15 21:33:05 +0300

    Update footer.tsx

--- components/Footer/footer.tsx
+++ components/Footer/footer.tsx
@@ -1,15 +1,15 @@
-import "./footer.scss";
+import styles from "@/Footer/footer.module.scss";
 
 export const Footer = () => {
   return (
-    <div className="footer-block">
-        <p className="copyright">
+    <div className={styles['footer-block']}>
+        <p className={styles.copyright}>
           © 2022–2026, WEB Studio - Разработка сайтов для предприятий
           <br />
           <br />
         </p>
       <a href="/">
-        <p className="policy">Политика сайта и обработка персональных данных</p>
+        <p className={styles.policy}>Политика сайта и обработка персональных данных</p>
       </a>
     </div>
   );
