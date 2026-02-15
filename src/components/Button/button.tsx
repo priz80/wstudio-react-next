4be6a28541298337b5da9b28195b8c0bd1e6f308
IGOR <127472053+priz80@users.noreply.github.com>
2026-02-16 01:26:23 +0300

    Create button.tsx

---
+++ src/components/Button/button.tsx
@@ -0,0 +1,21 @@
+import styles from "./button.module.scss";
+
+type ButtonProps = {
+  styleButton?: string;
+  fontButton?: string;
+  nameButton?: string;
+};
+
+const Button = ({
+  styleButton = styles.medium,     // используем классы из модуля
+  fontButton = styles["default-font"],
+  nameButton = "Кнопка",
+}: ButtonProps) => {
+  return (
+    <button type="button" className={`${styles.button} ${fontButton} ${styleButton}`}>
+      {nameButton}
+    </button>
+  );
+};
+
+export default Button;
