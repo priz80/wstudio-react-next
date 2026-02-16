a5ebe2edf34275f5617f8f06a808d1ce50011478
IGOR <127472053+priz80@users.noreply.github.com>
2026-02-16 23:06:00 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 'use client';
 
-import { Header } from "../components/Header/header";
-import { About } from "../components/About/about";
-import { Portfolio } from "../components/Portfolio/portfolio";
-import { Tarif } from "../components/Tarif/tarif";
-import { Forma } from "../components/Forma/forma";
-import { Contacty } from "../components/Contacty/contacty";
-import { Footer } from "../components/Footer/footer";
+import { Header } from "@/components/Header/header";
+import { About } from "@/components/About/about";
+import { Portfolio } from "@/components/Portfolio/portfolio";
+import { Tarif } from "@/components/Tarif/tarif";
+import { Forma } from "@/components/Forma/forma";
+import { Contacty } from "@/components/Contacty/contacty";
+import { Footer } from "@/components/Footer/footer";
 
 
 
