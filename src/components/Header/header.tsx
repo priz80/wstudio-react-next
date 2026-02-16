91aca07c22fbdc2f1a7d9bb8ad749e0d19228e14
IGOR <127472053+priz80@users.noreply.github.com>
2026-02-16 20:02:51 +0300

    Update header.tsx

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -3,6 +3,8 @@ import Button from "../Button/button";
 import headerStyles from "./headers.module.scss";
 import menuStyles from "./main-menu.module.scss";
 import burgerStyles from "./burger.module.scss";
+import imageMan from "public/images/man.png"
+import imageManSmall from "public/images/man-small.png"
 
 export const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -16,8 +18,8 @@ export const Header = () => {
           alt="lamp"
         />
         <div className="aside-line"></div>
-        <div className={headerStyles['man-container']}></div>
-        {/* <img src="img/header_img/man.png" className="man-container" alt="man" /> */}
+        <div className={headerStyles['man-container']}> <img src={imageMan.src} alt="Man" /> </div>
+        <div className={headerStyles['man-container-small']}> <img src={imageManSmall.src} alt="Man" /> </div>
         <div className={headerStyles.circle}>
           <div className={`${headerStyles.circle} ${headerStyles['circle-m']}`}>
             <div className={`${headerStyles.circle} ${headerStyles['circle-s']}`}></div>
