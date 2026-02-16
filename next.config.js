b04ad9874868e179e2a03584f218d776a5d2af1b
IGOR <127472053+priz80@users.noreply.github.com>
2026-02-16 23:05:28 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,25 +1,11 @@
-// next.config.js
-
-/* const isProd = process.env.NODE_ENV === "production"; */
-
-/* @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactCompiler: false,
-  output: "export",
-  // basePath: isProd ? "/wstudio-react-next" : "",
-  // assetPrefix: isProd ? "/wstudio-react-next/" : "",
+  output: 'export',
+  basePath: '/wstudio-react-next',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
-
-  /* experimental: {
-    externalDir: true,
-  }, */
-
-  /* webpack: (config, { dev, isServer }) => {
-    return config;
-  }, */
 };
 
-export default nextConfig;
+module.exports = nextConfig;
