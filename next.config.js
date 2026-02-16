d8aa3ddc85637561923283aaf7b7204ebc55a677
IGOR <127472053+priz80@users.noreply.github.com>
2026-02-16 23:51:43 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'export', // Генерация статики
+
+  // Базовый путь — обязательно для GitHub Pages
   basePath: '/wstudio-react-next',
+
+  // Важно: чтобы изображения и другие ассеты тоже использовали basePath
+  assetPrefix: '/wstudio-react-next/',
+
+  // Опционально: если используете trailing slash
   trailingSlash: false,
-  images: {
-    unoptimized: true,
-  },
 };
 
 module.exports = nextConfig;
