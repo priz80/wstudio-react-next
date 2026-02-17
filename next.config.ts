9549bcefec5d4db7e57209da3c9882bc4a08a328
IGOR <127472053+priz80@users.noreply.github.com>
2026-02-17 16:45:55 +0300

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,6 +0,0 @@
-const nextConfig = {
-  output: 'export', // Экспортируем как статические файлы
-  distDir: 'out',  // Папка сборки — dist вместо .next
-  trailingSlash: true, // Важно для GitHub Pages
-};
-module.exports = nextConfig;
