3602aa15f737fcb04e64adc203ca3de5d90e5b76
IGOR <127472053+priz80@users.noreply.github.com>
2026-02-17 16:45:50 +0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+const nextConfig = {
+  output: "export", // Экспортируем как статические файлы
+  distDir: "out", // Папка сборки
+  // trailingSlash: true, // Важно для GitHub Pages
+  //  basePath: "/wstudio-react-next", // ← Имя вашего репозитория только для деплоя
+  //  assetPrefix: "/wstudio-react-next/", // Префикс для статики только для деплоя
+};
+module.exports = nextConfig;
