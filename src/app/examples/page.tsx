cf439809ed52f6f3488fcf1eda1f8d116a091f43
IGOR <127472053+priz80@users.noreply.github.com>
2026-02-20 02:27:21 +0300

    Create page.tsx

---
+++ src/app/examples/page.tsx
@@ -0,0 +1,11 @@
+import ScreenshotSlider from '@/components/ScreenshotSlider/ScreenshotSlider';
+import Button from '@/components/Button/button';
+
+export default function Examples() {
+  return (
+    <div>
+      {/* <a href='/'><div><Button nameButton={'<'} /></div></a> */}
+        <ScreenshotSlider />
+    </div>
+  );
+}
