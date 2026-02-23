2bb85db493de98015ac2795ae262d53d5ffc8424
IGOR <127472053+priz80@users.noreply.github.com>
2026-02-23 16:27:34 +0300

    Update contacty.tsx

--- src/components/Contacty/contacty.tsx
+++ src/components/Contacty/contacty.tsx
@@ -2,61 +2,63 @@ import styles from "./contacty.module.scss";
 
 export const Contacty = () => {
   return (
-    <>
-      <div className={styles.container} id="contacty">
-        <div className={styles.content}>
+    <div className="container">
+      <div className='content' id="contacty">
+        {/* <div className={styles.content}> */}
           <div className={styles["title-contacty"]}>
             <h6>Контакты</h6>
           </div>
-        </div>
+        {/* </div> */}
         <div className={styles["ball-block_contacty"]}>
-          <a href="/">
-            {/* <div className={styles["ball-circle"]}> */}
-              <div className={styles.ball}>
-                
-                  {/* SVG content remains commented */}
-                
-              </div>
-            {/* </div> */}
-          </a>
-          <a href="/" target="blank_">
-            {/* <div className={styles["ball-circle"]}> */}
-              <div className={styles.ball}>
-                
-                  <img src="/icons/max.svg" alt="" />
-                
-              </div>
-            {/* </div> */}
-          </a>
-          <a href="https://t.me/Studio_WWW" target="blank_">
-            {/* <div className={styles["ball-circle"]}> */}
-              <div className={styles.ball}>
-                
-                  <img src="/icons/telegram.svg" alt="" />
-                
-              </div>
-            {/* </div> */}
-          </a>
-          <a href="/" target="blank_">
-            {/* <div className={styles["ball-circle"]}> */}
-              <div className={styles.ball}>
-                
-                  <img src="/icons/vk.svg" alt="" />
-                
-              </div>
-            {/* </div> */}
-          </a>
-          <a href="/">
-            {/* <div className={styles["ball-circle"]}> */}
-              <div className={styles.ball}>
-                
-                  <img src="/icons/gmail.svg" alt="" />
-                
-              </div>
-            {/* </div> */}
-          </a>
+          <div className={styles.ballBlock}>
+            <a href="/">
+              {/* <div className={styles["ball-circle"]}> */}
+                <div className={styles.ball}>
+                  
+                    {/* SVG content remains commented */}
+                  
+                </div>
+              {/* </div> */}
+            </a>
+            <a href="/" target="blank_">
+              {/* <div className={styles["ball-circle"]}> */}
+                <div className={styles.ball}>
+                  
+                    <img src="/icons/max.svg" alt="" />
+                  
+                </div>
+              {/* </div> */}
+            </a>
+            <a href="https://t.me/Studio_WWW" target="blank_">
+              {/* <div className={styles["ball-circle"]}> */}
+                <div className={styles.ball}>
+                  
+                    <img src="/icons/telegram.svg" alt="" />
+                  
+                </div>
+              {/* </div> */}
+            </a>
+            <a href="/" target="blank_">
+              {/* <div className={styles["ball-circle"]}> */}
+                <div className={styles.ball}>
+                  
+                    <img src="/icons/vk.svg" alt="" />
+                  
+                </div>
+              {/* </div> */}
+            </a>
+            <a href="/">
+              {/* <div className={styles["ball-circle"]}> */}
+                <div className={styles.ball}>
+                  
+                    <img src="/icons/gmail.svg" alt="" />
+                  
+                </div>
+              {/* </div> */}
+            </a>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
