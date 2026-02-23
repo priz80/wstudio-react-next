9e8d6f85f199e58b24f2159513293a48ceec5ab8
IGOR <127472053+priz80@users.noreply.github.com>
2026-02-23 22:00:23 +0300

    Update page.tsx

--- src/app/examples/page.tsx
+++ src/app/examples/page.tsx
@@ -3,7 +3,6 @@ import ScreenshotSlider from "@/components/ScreenshotSlider/ScreenshotSlider";
 export default function Examples() {
   return (
     <div>
-      {/* <a href='/'><div><Button nameButton={'<'} /></div></a> */}
       <ScreenshotSlider />
     </div>
   );
