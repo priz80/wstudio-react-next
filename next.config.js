0ed4ed9c8b959016938f511666aa28a768c39aef
IGOR <127472053+priz80@users.noreply.github.com>
2026-02-23 20:39:47 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 const nextConfig = {
   output: "export", // Экспортируем как статические файлы
+   images: {
+    unoptimized: true,
+  },
   distDir: "out", // Папка сборки
 };
 module.exports = nextConfig;
