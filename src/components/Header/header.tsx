3581bef268a7fa961e3a987bd6249c05f880dcbb
IGOR <127472053+priz80@users.noreply.github.com>
2026-05-20 11:54:34 +0300

    Update header.tsx

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -31,10 +31,10 @@ export const Header = () => {
   height={29} 
         />
         <div className="aside-line"></div>
-        {/* <div className={headerStyles['man-container']}>
+        <div className={headerStyles['man-container']}>
           <Image src="/images/man.png" alt="Man"  width={663} 
   height={844} />
-        </div> */}
+        </div>
         <div className={headerStyles['man-container-small']}>
           <Image src="images/man-small.svg" alt="Man"  width={241} 
   height={412} />
