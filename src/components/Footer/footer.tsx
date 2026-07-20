174671ed75565c51f6518607b211e49d9aa3e87a
IGOR <priz8090@gmail.com>
2026-07-20 04:08:40 +0300

    Update footer.tsx

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -11,9 +11,8 @@ export const Footer = () => {
       <a href="Link /">
         <p className={styles.policy}>Политика сайта и обработка персональных данных</p>
       </a>
+      <a href="https://fastvps.ru/c_4295ba5d6a21fdd9e4d2b8a7fad98400" target="_blank">Сайт работает на быстром VPS/VDS хостинге от FASTVPS</a>
     </div>
-    <div>
-        <a href="https://fastvps.ru/c_4295ba5d6a21fdd9e4d2b8a7fad98400" target="_blank">Сайт работает на быстром VPS/VDS хостинге от FASTVPS</a>
-      </div>
+    
   );
 };
