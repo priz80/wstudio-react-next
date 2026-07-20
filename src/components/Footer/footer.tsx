bc471f6eb675943d01cd08f4f0f6df0aec0fb08e
IGOR <priz8090@gmail.com>
2026-07-20 08:31:23 +0300

    Update footer.tsx

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -2,17 +2,23 @@ import styles from "./footer.module.scss";
 
 export const Footer = () => {
   return (
-    <div className={styles['footer-block']}>
-        <p className={styles.copyright}>
-          © 2022–2026, WEB Studio - Разработка сайтов для предприятий
-          <br />
-          <br />
-        </p>
+    <div className={styles["footer-block"]}>
+      <p className={styles.copyright}>
+        © 2022–2026, WEB Studio - Разработка сайтов для предприятий
+        <br />
+        <br />
+      </p>
       <a href="Link /">
-        <p className={styles.policy}>Политика сайта и обработка персональных данных</p>
+        <p className={styles.policy}>
+          Политика сайта и обработка персональных данных
+        </p>
       </a>
-      <a href="https://fastvps.ru/c_4295ba5d6a21fdd9e4d2b8a7fad98400" target="_blank">Сайт работает на быстром VPS/VDS хостинге от FASTVPS</a>
+      <a
+        href="https://fastvps.ru/c_4295ba5d6a21fdd9e4d2b8a7fad98400"
+        target="_blank"
+      >
+        <p className={styles.policy}>Сайт работает на быстром VPS/VDS хостинге от FASTVPS</p>
+      </a>
     </div>
-    
   );
 };
